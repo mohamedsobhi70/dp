145fe1e53bcb25294456d6af1d029332c7ba047a
mohamedsobhi70 <sobhi7070@gmail.com>
2023-02-24 21:03:34 +0200

    final

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,68 +5,68 @@ darkBtn.addEventListener("click", function () {
   sticky()
 })
 
-$('.carousel-1').owlCarousel({
-  center: true,
-  loop: true,
-  responsive: {
+// $('.carousel-1').owlCarousel({
+//   center: true,
+//   loop: true,
+//   responsive: {
 
-    10: {
-      items: 3,
-      margin: 24,
+//     10: {
+//       items: 3,
+//       margin: 24,
 
-    }
-    , 768: {
-      items: 5,
-      margin: 24,
+//     }
+//     , 768: {
+//       items: 5,
+//       margin: 24,
 
-    },
-    1024: {
-      items: 7,
-      margin: 30,
+//     },
+//     1024: {
+//       items: 7,
+//       margin: 30,
 
-    },
-    1650: {
-      items: 8,
-      margin: 30,
-
-
-    }
-  }
-});
-$('.carousel-2').owlCarousel({
-  center: true,
-  loop: true,
-  responsive: {
-
-    10: {
-      items: 1.3,
-      margin: 24,
-
-    }
-    , 768: {
-      items: 2,
-      margin: 24,
-
-    },
-    1024: {
-      items: 3.75,
-      margin: 30,
-
-    },
-    1650: {
-      items: 5.5,
-      margin: 30,
+//     },
+//     1650: {
+//       items: 8,
+//       margin: 30,
 
 
-    },
-    2650: {
-      items: 7.5,
-      margin: 30,
+//     }
+//   }
+// });
+// $('.carousel-2').owlCarousel({
+//   center: true,
+//   loop: true,
+//   responsive: {
+
+//     10: {
+//       items: 1.3,
+//       margin: 24,
+
+//     }
+//     , 768: {
+//       items: 2,
+//       margin: 24,
+
+//     },
+//     1024: {
+//       items: 3.75,
+//       margin: 30,
+
+//     },
+//     1650: {
+//       items: 5.5,
+//       margin: 30,
 
 
-    }
-  }
-});
+//     },
+//     2650: {
+//       items: 7.5,
+//       margin: 30,
+
+
+//     }
+//   }
+// });
 
 
 
@@ -113,9 +113,9 @@ window.addEventListener("scroll", () => {
   sticky();
   if (window.screen.width > 768) {
     let val = window.scrollY;
-    document.querySelector(".paralex-element-1").style.left = val * .10 + "px";
-    document.querySelector(".paralex-element-2").style.right = val * .10 + "px";
-    document.querySelector(".paralex-element-3").style.left = val * .10 + "px";
+    document.querySelector(".paralex-element-1").style.left = val * .05 + "px";
+    document.querySelector(".paralex-element-2").style.right = val * .05 + "px";
+    document.querySelector(".paralex-element-3").style.left = val * .05 + "px";
   }
 
   let workSec = document.getElementById("work");
@@ -126,23 +126,26 @@ window.addEventListener("scroll", () => {
   let servicesLi = document.querySelector(".nav-link a.services");
   let pricingLi = document.querySelector(".nav-link a.pricing");
   if (window.scrollY > workSec.offsetTop - 200) {
-    workLi.classList.add("font-bold");
+    workLi.classList.add("font-bold", "dark:text-white", "text-[#111827]");
     workLi.classList.remove("font-medium");
-    heroLi.classList.remove("font-bold");
-    servicesLi.classList.remove("font-bold");
+    heroLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
+    heroLi.classList.add("text-[#6B7280]", "dark:text-[#9CA3AF]");
+    servicesLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
     pricingLi.classList.remove("font-bold");
     if (window.scrollY > servicesSec.offsetTop - 200) {
-      servicesLi.classList.add("font-bold");
+      servicesLi.classList.add("font-bold", "dark:text-white", "text-[#111827]");
       servicesLi.classList.remove("font-medium");
-      heroLi.classList.remove("font-bold");
-      workLi.classList.remove("font-bold");
-      pricingLi.classList.remove("font-bold");
+      heroLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
+      heroLi.classList.add("text-[#6B7280]", "dark:text-[#9CA3AF]");
+      workLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
+      pricingLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
       if (window.scrollY > pricingSec.offsetTop - 200) {
-        pricingLi.classList.add("font-bold");
+        pricingLi.classList.add("font-bold", "dark:text-white", "text-[#111827]");
         pricingLi.classList.remove("font-medium");
-        heroLi.classList.remove("font-bold");
-        workLi.classList.remove("font-bold");
-        servicesLi.classList.remove("font-bold");
+        heroLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
+        heroLi.classList.add("text-[#6B7280]", "dark:text-[#9CA3AF]");
+        workLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
+        servicesLi.classList.remove("font-bold", "dark:text-white", "text-[#111827]");
       }
     }
   }
@@ -155,6 +158,47 @@ window.addEventListener("scroll", () => {
 });
 
 
-// $(".carousel-2 .owl-stage").append('<svg class="svg-mask absolute bottom-[-47px] left-0 dark:fill-darkclr fill-[#FFFAF4] z-10 " width="100%" height="75" viewBox="0 0 1600 75" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 75H1601V30.5C1601 30.5 1167.74 0.5 798.5 0.5C453.081 0.5 0 30.5 0 30.5V75Z" /></svg>')
-// $(".carousel-2 .owl-stage").append('<svg class="svg-mask absolute top-[-58px] left-0 dark:fill-darkclr fill-[#FFFAF4] z-10 " width="100%"height="85" viewBox="0 0 1600 85" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1600 0.0001297L7.43092e-06 -1.01767e-05L2.6382e-06 54.8224C2.6382e-06 54.8224 434.03 84.9999 801.993 84.9999C1146.22 85 1600 54.8225 1600 54.8225L1600 0.0001297Z" />/svg>')
-// $(".svg-mask").css("width","100% !important")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(".carousel-1 ").flickity({
+  autoPlay: true,
+  cellAlign: "center",
+  wrapAround: true,
+  pageDots: false,
+  prevNextButtons: false,
+  imagesLoaded: true,
+  setGallerySize: true,
+});
+let $carousel = $(".carousel-2 ").flickity({
+  autoPlay: true,
+  cellAlign: "center",
+  wrapAround: true,
+  pageDots: false,
+  prevNextButtons: false,
+  imagesLoaded: true,
+  setGallerySize: true,
+});
+$(".carousel-2 .carousel-cell.is-selected").css("border", "1px solid #8859FA");
+$carousel.on('change.flickity', () => {
+  $(".carousel-2 .carousel-cell.is-selected").css({ "border-radius": "16px", "border": "1px solid #8859FA", "box-shadow": "0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03)" });
+  $(".carousel-2 .carousel-cell:not(.is-selected").css({ "border-radius": "0px", "border": "none", "box-shadow": "none" });
+});
+
+// carousel-cell w-[400px] h-[400px] px-[15px] is-selected"border: 1px solid #8859FA;
+// box-shadow: 0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03);
